c500bf8542de3e66cfae89d71adfa76fd060ea94
Brian Reber <reber.brian@gmail.com>
2010-09-10 04:27:02 +0000

    Delete persistent all
    git-svn-id: http://cyride-android.googlecode.com/svn/trunk/appengine@24 7b41c333-8d55-7cf5-d94c-7ef30ea08043

--- src/org/reber/Cyride/DeleteAll.java
+++ src/org/reber/Cyride/DeleteAll.java
@@ -19,8 +19,8 @@ public class DeleteAll extends HttpServlet {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		try {
 			Query q = pm.newQuery(Route.class);
-			q.setFilter("routeId == 0 OR routeId == 1 OR routeId == 3 OR routeId == 4");
-			q.setFilter("day == 0");
+//			q.setFilter("routeId == 0 OR routeId == 1 OR routeId == 3 OR routeId == 4");
+//			q.setFilter("day == 0");
 			try {
 				q.deletePersistentAll();
 			} catch (DeadlineExceededException e) {
