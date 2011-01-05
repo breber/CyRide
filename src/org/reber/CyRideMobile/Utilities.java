898ed45584b38c07de753ccbcb97def0647213a0
Brian Reber <reber.brian@gmail.com>
2011-01-05 01:57:20 +0000

    Adding copyright
    git-svn-id: http://cyride-android.googlecode.com/svn/trunk/android@50 7b41c333-8d55-7cf5-d94c-7ef30ea08043

--- src/org/reber/CyRideMobile/Utilities.java
+++ src/org/reber/CyRideMobile/Utilities.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2011 Brian Reber
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation,
+ * advertising materials, and other materials related to such
+ * distribution and use acknowledge that the software was developed
+ * by Brian Reber.  
+ * THIS SOFTWARE IS PROVIDED 'AS IS' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 package org.reber.CyRideMobile;
 
 import java.util.HashMap;
@@ -15,16 +29,9 @@ public class Utilities {
 	public static String PACKAGE = "org.reber.CyRideMobile";
 	public static String DATA_URL = "http://cyridesql.appspot.com/getroutes";
 	
-	/**
-	 * Gets the image resource that matches the given color/route string
-	 * 
-	 * @param color
-	 * @return
-	 * The resource id for the color image
-	 */
-	public static int getImageResource(String color) {
-		Map<String, Integer> colors = new HashMap<String, Integer>();
-		
+	private static Map<String, Integer> colors = new HashMap<String, Integer>();
+	
+	static {
 		colors.put("Red", R.drawable.red);
 		colors.put("Blue", R.drawable.blue);
 		colors.put("Green", R.drawable.green);
@@ -39,7 +46,16 @@ public class Utilities {
 		colors.put("Gray", R.drawable.gray);
 		colors.put("Cardinal", R.drawable.cardinal);
 		colors.put("Gold", R.drawable.gold);
-		
+	}
+	
+	/**
+	 * Gets the image resource that matches the given color/route string
+	 * 
+	 * @param color
+	 * @return
+	 * The resource id for the color image
+	 */
+	public static int getImageResource(String color) {
 		for (String s : color.split("\\s|/")) {
 			if (colors.containsKey(s)) {
 				return colors.get(s);
